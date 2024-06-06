bb3235073720cc1d9d49ca5f165c5b3fbbe8dff0
Paul Kinlan <paul.kinlan@gmail.com>
2024-06-06 13:58:25 +0000

    Add `model` check and package information

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,20 @@
 import { computed as l } from "@preact/signals-core";
-function s(o, ...n) {
+function s(n, ...r) {
+  if (!("model" in globalThis))
+    throw new Error(`The 'prompt' API is not enabled in your browser. 
+
+    Please use at least Chrome 127 and enable the following features:
+    + chrome://flags/#prompt-api-for-gemini-nano
+    + chrome://flags/#optimization-guide-on-device-model "Enable Bypass" 
+    `);
   const a = model.createTextSession();
   return l(async () => {
-    const r = [o[0]];
-    for (let e = 0; e < n.length; e++) {
-      let t = n[e];
-      "brand" in t && typeof t.brand == "symbol" && Symbol.keyFor(t.brand) === "preact-signals" && (t = t.value), r.push(await t, o[e + 1]);
+    const t = [n[0]];
+    for (let e = 0; e < r.length; e++) {
+      let o = r[e];
+      "brand" in o && typeof o.brand == "symbol" && Symbol.keyFor(o.brand) === "preact-signals" && (o = o.value), t.push(await o, n[e + 1]);
     }
-    return a.then((e) => e.prompt(r.join("")));
+    return a.then((e) => e.prompt(t.join("")));
   });
 }
 export {
